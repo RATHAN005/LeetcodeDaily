cbe28c9c16a5eac437f226fabc005004d4936d35
RATHAN K <167230786+RATHAN005@users.noreply.github.com>
2026-04-19 12:06:39 +0530

    Time: 2 ms (90.32%), Space: 44.3 MB (28.18%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,65 +1,42 @@
 class Solution {
-    class orange
-    {
-        int i;
-        int j;
-        int t;
-        orange(int i,int j,int t)
-        {
-            this.i=i;
-            this.j=j;
-            this.t=t;
-        }
-    }
     public int orangesRotting(int[][] grid) {
-        Queue<orange>q=new LinkedList<>();
-        boolean [][]vis=new boolean [grid.length][grid[0].length];
-        int freshCount=0;
-        for(int i=0;i<grid.length;i++)
-        {
-            for(int j=0;j<grid[0].length;j++)
-            {
-                if(grid[i][j]==1)
-                    freshCount++;
-                else if(grid[i][j]==2)
-                {
-                    q.add(new orange(i,j,0));
-                    vis[i][j]=true;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==2){
+                    q.offer(new int[]{i,j});
                 }
-                else
-                    continue;
+                else if(grid[i][j]==1){
+                    fresh++;
+                }
             }
         }
-        int time=0;
-        int [][]dir={{0,1},{1,0},{0,-1},{-1,0}};
-        while(q.size()>0)
-        {
-            orange fnt= q.remove();
-            vis[fnt.i][fnt.j]=true;
-            time=Math.max(time,fnt.t);
-            for(int []d:dir)
-            {
-                int nr=fnt.i+d[0];
-                int nc=fnt.j+d[1];
-                if(isValid(nr,nc,grid) && grid[nr][nc]==1 && (vis[nr][nc]==false))
-                {
-                    freshCount--;
-                    vis[nr][nc]=true;
-                    q.add(new orange(nr,nc,fnt.t+1));
+        int minute = 0;
+        int[][]dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.isEmpty() && fresh > 0){
+            int size = q.size();
+            minute++;
+
+            for(int i=0; i < size; i++){
+                int[] curr = q.poll();
+
+                for(int[] d : dir){
+                    int ni = curr[0] + d[0];
+                    int nj = curr[1] + d[1];
+                if(ni >= 0 && nj >=0 && ni < m && nj < n && grid[ni][nj]==1){
+                    grid[ni][nj]=2;
+                    q.offer(new int[] {ni,nj});
+                    fresh--;
+                    }
                 }
             }
-        }    
-        if(freshCount==0)
-            return time;
-        else
-            return -1;    
-    }
-
-    public boolean isValid(int r,int c,int [][]grid)
-    {
-        if(r>=0 && r<grid.length && c>=0 && c<grid[0].length)
-            return true;
-        else
-            return false;
+        }
+        return(fresh ==0 )? minute : -1;
     }
 }
